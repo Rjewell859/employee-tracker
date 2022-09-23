b6234ef68c573ccae8d3d0b877693f5acf25c07f
Headwall Forest <headwallforest27@gmail.com>
2022-09-23 14:58:09 -0400

    Working on viewAllEmployees next.

--- index.js
+++ index.js
@@ -6,11 +6,40 @@ const db = mysql.createConnection(
       host: 'localhost',
       user: 'root',
       password: 'root',
-      database: 'employees_db'
+      database: 'employee_db'
     },
     console.log(`Connected to the employees_db database.`)
   );
 
-  db.query('', function (err, results) {
+  db.query('SELECT * FROM departments', function (err, results) {
     console.log(results);
   });
+
+  db.query('SELECT * FROM employees', function (err, results) {
+    console.log(results);
+  });
+  db.query('SELECT * FROM roles', function (err, results) {
+    console.log(results);
+  });
+
+var viewAllEmployees = function() {
+
+  
+}
+
+
+
+
+
+
+
+
+  var tableTest = function () {
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+const me = new Person("John", "Smith");
+  console.table( [me],)
+}
+tableTest();
