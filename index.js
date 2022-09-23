e32e249862a2c3506a2757cb9689aa7ace5ea244
Headwall Forest <headwallforest27@gmail.com>
2022-09-23 15:21:43 -0400

    Progress to db.query

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: 'root',
-      database: 'employee_db'
-    },
-    console.log(`Connected to the employees_db database.`)
-  );
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'employee_db'
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+db.query(`SELECT
+employees.first_name AS "First Name", roles.title AS Title
+FROM employees
+JOIN roles ON employees.role_id = roles.id;`, function (err, results) {
+  console.table(results);
+});
 
-  db.query('SELECT * FROM departments', function (err, results) {
-    console.log(results);
-  });
+db.query(`SELECT
+  roles.title AS "Role", departments.department_name AS Department
+  FROM roles
+  JOIN departments ON roles.department_id = departments.id;`, function (err, results) {
+  console.table(results);
+});
 
+
+
+var viewAllEmployees = function () {
   db.query('SELECT * FROM employees', function (err, results) {
-    console.log(results);
-  });
-  db.query('SELECT * FROM roles', function (err, results) {
-    console.log(results);
+    console.table(results);
   });
 
-var viewAllEmployees = function() {
-
-  
 }
+viewAllEmployees();
 
 
 
@@ -34,12 +40,12 @@ var viewAllEmployees = function() {
 
 
 
-  var tableTest = function () {
-function Person(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-}
-const me = new Person("John", "Smith");
-  console.table( [me],)
+var tableTest = function () {
+  function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  const me = new Person("John", "Smith");
+  console.table([me], )
 }
 tableTest();
