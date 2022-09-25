2d4f0ba558be6e1fadc21d7024fd9786351c2c08
Headwall Forest <headwallforest27@gmail.com>
2022-09-25 19:31:49 -0400

    Made a ton of changes with queries, adding employee. Getting manager and role name from ids.

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
+var managers = []
+var rolesArray = []
 const questions = [{
   type: 'list',
   name: 'menu',
   message: 'What would you like to do?',
   choices: ['Add an employee']
-},
-];
+}, ];
 const empQuestions = [{
-  type: 'input',
-  name: 'firstName',
-  message: 'What is their first name?',
-},
-{
-  type: 'input',
-  name: 'lastName',
-  message: 'What is their last name?',
-},
-{
-  type: 'list',
-  name: 'role',
-  message: 'What is their role?',
-  choices: ['1', '2', '3', '4']
-},
-{
-  type: 'list',
-  name: 'manager',
-  message: 'Who is their manager?',
-  choices: ['1', '2', '3', '4']
-},
+    type: 'input',
+    name: 'firstName',
+    message: 'What is their first name?',
+  },
+  {
+    type: 'input',
+    name: 'lastName',
+    message: 'What is their last name?',
+  },
+  {
+    type: 'list',
+    name: 'role',
+    message: 'What is their role?',
+    choices: rolesArray
+  },
+  {
+    type: 'list',
+    name: 'manager',
+    message: 'Who is their manager?',
+    choices: managers
+  },
 ];
 
 const db = mysql.createConnection({
@@ -40,6 +41,30 @@ const db = mysql.createConnection({
   },
   console.log(`Connected to the employees_db database.`)
 );
+
+const getRoles = function () {
+  db.query(`SELECT * FROM roles`, function (err, results) {
+    for (let i = 0; i < results.length; i++) {
+      rolesArray.push(results[i].title)
+    }
+  })
+}
+const getManagers = function () {
+  db.query(`
+  SELECT *
+  FROM employees
+  WHERE role_id = 5`, function (err, results) {
+    
+    for (let i = 0; i < results.length; i++) {
+      managers.push(results[i].first_name + " " + results[i].last_name)
+    }
+  })
+}
+
+
+getRoles()
+getManagers()
+
 db.query(`SELECT
 employees.first_name AS "First Name", roles.title AS Title
 FROM employees
@@ -57,67 +82,87 @@ db.query(`SELECT
 
 
 var viewAllEmployees = function () {
-  db.query('SELECT * FROM employees', function (err, results) {
+  db.query(`SELECT
+  e.id AS "Employee ID",
+  e.first_name AS "First Name",
+  e.last_name AS "Last Name",
+  roles.title AS Title,
+  departments.department_name AS Department,
+  roles.salary AS Salary,
+  CONCAT((m.first_name),(' '), (m.last_name)) AS Manager
+FROM employees e
+JOIN roles ON e.role_id = roles.id
+JOIN departments ON roles.department_id = departments.id
+INNER JOIN employees m ON e.manager_id = m.id;`, function (err, results) {
     console.table(results);
   });
 
 }
 viewAllEmployees();
 
-var nextQuestions = function(response) {
+var nextQuestions = function (response) {
   response = JSON.parse(response)
   switch (response.menu) {
     case 'Add an employee':
-        askEmployeeQuestions();
-        break;
+      askEmployeeQuestions();
+      break;
     default:
-        return;
-}
-  
-}
-
-
-
-
-
-
-var tableTest = function () {
-  function Person(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+      return;
   }
-  const me = new Person("John", "Smith");
-  console.table([me], )
+
 }
-tableTest();
+
 
 var askQuestions = function () {
   inquirer
-      .prompt(questions)
-      .then((data) =>
-          nextQuestions(JSON.stringify(data))
-      )
+    .prompt(questions)
+    .then((data) =>
+      nextQuestions(JSON.stringify(data))
+    )
 }
 
 var askEmployeeQuestions = function () {
   inquirer
-      .prompt(empQuestions)
-      .then((data) =>
-          saveEmployee(JSON.stringify(data))
-      )
+    .prompt(empQuestions)
+    .then((data) =>
+      saveEmployee(JSON.stringify(data))
+    )
 }
 
 var saveEmployee = function (response) {
   response = JSON.parse(response);
+  let currentManager = ''
+  let currentRole = ''
+  let managerName = response.manager.split(" ");
+  db.query(`SELECT id FROM roles WHERE('${response.role}' = roles.title)`, function (err, reresult) {
+    if (err) {
+      console.log(err);
+    }
+    
+ (db.query(`SELECT id FROM employees WHERE ('${managerName[0]}'= employees.first_name AND '${managerName[1]}' = employees.last_name)`, function (err, result) {
+
+  if (err) {
+    console.log(err);
+  }
+  currentManager = result[0].id
+  console.log(currentManager)
+  console.log(currentManager)
   db.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id)
-  VALUES ("${response.firstName}", "${response.lastName}", ${response.role}, ${response.manager})`, function (err, result) {
+  VALUES ("${response.firstName}", "${response.lastName}", ${reresult[0].id}, ${currentManager})`, function (err, result) {
     if (err) {
       console.log(err);
     }
     console.log(result);
+    console.log('reached')
+    viewAllEmployees();
+    getRoles()
+getManagers()
   });
-  console.log('reached')
-  viewAllEmployees();
+}))
+  });
+  
+
+
 }
 
 function init() {
